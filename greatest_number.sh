d8e0a9e53b1cce4b3b73ec07dd03123a01ba01ac
rahmat <rahmatiqbal123@gmail.com>
2021-10-07 16:05:23 +0530

    to find greatest number

--- greatest_number.sh
+++ greatest_number.sh
@@ -5,4 +5,12 @@ read -p "Enter the first number" a
 read -p "Enter the second number" b
 read -p "Enter the third number" c
 
-if (( a -gt b)) && (( a -gt c))
+if [[ a -gt b ]] && [[ a -gt c ]];
+then 
+	echo $a
+elif [[ b -gt a ]] && [[ b -gt c ]];
+then
+	echo $b
+else
+	echo $c
+fi
