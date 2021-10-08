918628c0a1a0a65f07a13d2c859ed3396a97054d
rahmat <rahmatiqbal123@gmail.com>
2021-10-08 10:55:34 +0530

    calculator

--- menu_calc.sh
+++ menu_calc.sh
@@ -5,3 +5,28 @@ read -p "Enter second Number: " b
 
 echo "choose the option"
 echo "1 for Addition"
+echo "2 for Subtration"
+echo "3 for multiplication"
+echo "4 for Division"
+
+read option
+
+if [[ $option -eq 1 ]]
+then
+	c=$((a+b))
+	echo $c
+elif [[ $option -eq 2 ]]
+then
+	c=$((a-b))
+	echo $c
+elif [[ $option -eq 3 ]]
+then
+	c=$((a*b))
+	echo $c
+elif [[ $option -eq 4 ]]
+then
+	c=$((a/b))
+	echo $c
+else
+	echo "Enter Right Option"
+fi
